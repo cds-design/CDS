9b19497e7eb857db53cfd073259d92e0c4c9febc
Rajaniraiyn R <72294760+Rajaniraiyn@users.noreply.github.com>
2023-05-06 02:07:43 +0530

    hjvj

--- packages/continuum.bak/src/styles/globals.css.ts
+++
@@ -1,8 +0,0 @@
-import { css } from "lit";
-import variables from "./_variables.css";
-import standardize from "./_standardize.css";
-
-export default css`
-  ${variables}
-  ${standardize}
-`;
--- packages/continuum.bak/types/WC.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement, PropertyValueMap } from "lit";
-export declare class WC extends LitElement {
-    className: string;
-    protected updated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void;
-}
