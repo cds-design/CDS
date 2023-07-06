bd20b30913695b0c72349bf8c26bb78ef0669308
Rajaniraiyn R <72294760+Rajaniraiyn@users.noreply.github.com>
2023-07-06 17:30:55 +0530

    sync

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Options} */
+const config = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'strict',
+    endOfLine: 'lf',
+    quoteProps: 'as-needed',
+    embeddedLanguageFormatting: 'auto',
+    singleAttributePerLine: true,
+};
+
+export default config;
